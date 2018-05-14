75bdc2669f93a9453db3d90720fe38cccb8cc714
François Gagnon <f_gagnon@videotron.ca>
2018-05-14 10:26:57 -0400

    Initial Commit

---
+++ Aldo_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aldo_Test
+//
+//  Created by François Gagnon on 18-05-14.
+//  Copyright © 2018 FoG Développement Mobile Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
